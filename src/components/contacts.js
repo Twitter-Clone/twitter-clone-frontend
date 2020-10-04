7a7ebbc88bfe63b9527d18fa72afec98f2b42a33
Matthew Knop <maknop@pop-os.localdomain>
2020-10-04 12:53:45 -0600

    Removed comments.

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -10,11 +10,11 @@ const Contacts = ({ contacts }) => {
             <center><h1>Twitter Clone App</h1></center>
             {contacts.map((contact) => (
                 <div class="card">
-                    <!--<div class="card-body">
+                    <div class="card-body">
                         <h5 class="card-title">{contact.name}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">{contact.email}</h6>
                         <p class="card-text">{contact.company.catchPhrase}</p>
-                    </div>-->
+                    </div>
                     <div>
                         <form>
                             <label>Enter your email</label>
