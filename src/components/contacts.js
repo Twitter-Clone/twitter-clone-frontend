acc5927da599efcc09edecc0b171329cca565ece
Matthew Knop <maknop@pop-os.localdomain>
2020-10-04 12:43:53 -0600

    Added simple form.

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -7,13 +7,21 @@ import React from 'react'
 const Contacts = ({ contacts }) => {
     return (
         <div>
-            <center><h1>Contact List</h1></center>
+            <center><h1>Twitter Clone App</h1></center>
             {contacts.map((contact) => (
                 <div class="card">
-                    <div class="card-body">
+                    <!--<div class="card-body">
                         <h5 class="card-title">{contact.name}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">{contact.email}</h6>
                         <p class="card-text">{contact.company.catchPhrase}</p>
+                    </div>-->
+                    <div>
+                        <form>
+                            <label>Enter your email</label>
+                            <input type="text">
+                            <label>Enter your twitter handle</label>
+                            <input type="text">
+                        </form>
                     </div>
                 </div>
             ))}
