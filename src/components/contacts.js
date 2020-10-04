c3b127c5fa9a9d5bcb3ad800bbb7bfb086a42ff6
Matthew Knop <maknop@pop-os.localdomain>
2020-10-04 13:23:01 -0600

    Added forms.

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -12,9 +12,9 @@ const Contacts = ({ contacts }) => {
                 <div>
                      <form>
                          <label>Enter your email</label>
-                         <input type="text"/>
+                         <input type="text"/><br /><br />
                          <label>Enter your twitter handle</label>
-                         <input type="text"/>
+                         <input type="text"/><br /><br />
                          <input type="submit" value="Submit" />
                      </form>
                 </div>
