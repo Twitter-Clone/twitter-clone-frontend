e2dc60abbeb0d34b82c1b508e9c2293074dc8c64
Doug Mellon <68387187+dougmellon@users.noreply.github.com>
2020-10-15 19:16:23 -0600

    Adjusted for our API

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -22,9 +22,8 @@ const Contacts = ({ contacts }) => {
             {contacts.map((contact) => (
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">{contact.name}</h5>
+                        <h5 class="card-title">{contact.twitterhandle}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">{contact.email}</h6>
-                        <p class="card-text">{contact.company.catchPhrase}</p>
                     </div>
                 </div>
             ))}
