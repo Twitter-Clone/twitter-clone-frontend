47f940073548d30812b30b9260873b0134dd245f
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-10-19 03:19:12 -0600

    Created profile page.

---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Profile() {
+    return (
+        <div className="profile">
+            <h1>Did it work?</h1>
+        </div>
+    );
+}
+
+export default Profile;
