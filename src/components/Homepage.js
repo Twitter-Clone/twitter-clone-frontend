99c070ca4510a9379c875e57014bd5924c9741fb
doug mellon <dm@dougmellon.com>
2020-10-31 22:52:53 -0600

    Fixed styling issues for the sidebar and main feed.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,14 +3,12 @@ import React from 'react'
 import '../App.css';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
-import Header from './Header';
 
 function Homepage() {
     return (
         <div className="app">
-            <Sidebar />
-            <Header />    
-            <Feed />
+                <Sidebar />
+                <Feed />
         </div>
     );
 }
