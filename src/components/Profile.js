c98a217996fb822401eb9844ca46658285dd2fc6
Matthew Knop <knopmatthew93@gmail.com>
2020-11-19 16:34:39 -0700

    Worked on user tweets for their profile.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,91 +1,178 @@
 import React, {Component} from 'react';
 
 import Sidebar from '../components/Sidebar';
+import Feed from './middleware/Feed';
 
-export default class Profile extends React.Component {
+export default class Profile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username:'',
-            email:''
+            tweets: [],
+            users: [],
+            word: '',
+            new: ''
         }
-    }
-    
-    componentWillMount() {
-        this.setState({
-            isMenuOpened: false
-        })
+        var newTweet = '';
     }
 
-    componentDidMount() {
-        
+    async componentDidMount() {
+        await this.TweetList();
+        await this.UserList();
     }
 
-    /*get_username() {
-        var username = this.state.username;
-        fetch('http://157.245.160.185:8000/tcapi/users/', {
-            method: 'GET',
+    async TweetList() {
+        await fetch('http://157.245.160.185:8000/tcapi/api/posts/', {
             headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify({
-                username
-            })
+                'Content-Type': 'application/json',
+                'Authorization': `JWT ${this.props.data["logged_in"]}`
+            }
+        })
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({ tweets: data })
+        })
+        .catch(console.log);
+    }
+
+    async UserList() {
+        await fetch('http://157.245.160.185:8000/tcapi/api/users/', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `JWT ${this.props.data["logged_in"]}`
+            }
+        })
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({ users: data })
+        })
+        .catch(console.log);
+    }
+
+    // there was an a before increment?
+    increment() {
+        // Attempting to fix the issue of no first tweets being sent
+        this.setState({
+            word: ''
+        });
+
+        this.setState({
+            word: this.state.new,
+            new: '',
+        });
+        
+        this.newTweet = this.state.new;
+        this.sendToDatabase(this.newTweet);
+    }
+
+    getUserId(username){
+        for(var i = 0; i < this.state.users.length; i++){
+            console.log("This is in getUserId: " + username);
+            if(this.state.users.username === username){
+                return this.state.users[i].id;
+            }
         }
-    }*/
-    
+    }
+
+    sendToDatabase(newTweet){
+
+        // Get users ID
+        const id = this.getUserId(this.props.data["username"]);
+
+        // Generate a random postid
+        var newPostId = Math.floor(Math.random() * Math.floor(10000))
+
+        // Create data object (postid, tweet, userid)
+        var newTweetObject = {
+            "postid": newPostId,
+            "tweet": newTweet,
+            "userid_id": id
+        }        
+
+        // Send object to the server
+        try {
+            fetch('http://157.245.160.185:8000/tcapi/api/postnew/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `JWT ${this.props.data["logged_in"]}`
+                },
+                body: JSON.stringify(newTweetObject)
+            }) 
+        } catch(e){
+            console.log('This is from the fetch of sendToDatabase: ' + e);
+        }
+
+        // Reload page for new feed with added tweet
+        window.location.reload(false);
+    }
+
+    handleChange(value) {
+        this.setState({
+            new: value
+        });
+    }
+
+    // findUsername = (id) => this.state.users.find(({ userid_id }) => userid_id === id);
+
+    findUsername(_id){
+        for(var i = 0; i < this.state.users.length; i++){
+            if(this.state.users[i].id === _id){
+                return this.state.users[i].username
+            }
+        }
+    }
+
+    tweetsCurrentUser() {
+        var newArray = [];
+        var currentUserTweets = [];
+
+        //// newArray = this.state.tweets.map(item => item)
+        var newArray = Object.values(this.state.tweets);
+        var currentUserId = this.getUserId(this.state.users.username)
+        
+        for (const i of newArray.entries()) {
+            console.log(this.state.users.username)
+            if (currentUserId == this.state.tweets.userid_id) {
+                currentUserTweets = newArray[i];
+            }
+        }
+
+        return newArray;
+    }
+
     render() {
 
-        return (
-            /*<OffCanvas
-                width={100}
-                transitionDuration={300}
-                isMenuOpened={this.state.isMenuOpened}
-                position={"left"}
-                effect={"overlay"}
-            >
-            <OffCanvasBody 
-                style={{ fontSize: "12px" }}
-            >
-                <h1>your_username</h1>
-                <>
-                    { <Sidebar /> }
-                    <h1>
-                        <a href="#" onClick={this.handleClick.bind(this)}>
-                            Profile
-                        </a>{" "}
-                    </h1>
-                </>
-            </OffCanvasBody>   
-            <OffCanvasMenu>
-                <p>your_username</p>
-                    
-                        <script>document.getElementById('btn').onclick = function() {
-                                // ? val = document.getElementById('imagename').value,
-                                //src = 'https://assets.newatlas.com/dims4/default/572b515/2147483647/strip/true/crop/1620x1080+150+0/resize/1200x800!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2Farchive%2Fblue-whale-1.jpg',
-                                //img = document.createElement('img')
-                            }
-                            img.src = src;
-                            document.body.appendChild(img);
-                        </script>
-                        
-                        <h1>
-                            <a href="#" onClick={this.handleClick.bind(this)}>
-                                Your Username Name
-                            </a>{" "}
-                        </h1>
-                
-            </OffCanvasMenu>
-            </OffCanvas> */
-            <>
-                <Sidebar />
-                <h1>Profile</h1>
-            </>
-        )
+        var onOff = true;
+
+        if(onOff){
+
+            var currentUserTweets = this.tweetsCurrentUser();
+            var newArrayReversed = currentUserTweets.reverse();
+            //console.log(currentUserTweets);
+        }
+
+        if(onOff){
+            return (
+                <div>
+                    <div class="tweetInput">
+                        <center>
+                            <textarea placeholder="Type your tweet..." rows="5" cols="50" value={this.state.new} onChange={(e) =>this.handleChange(e.target.value)}>Enter your tweet</textarea>
+                        </center>
+                        <input type="submit" value="Tweet" onClick={() => this.increment()} />
+                    </div>
+                    <div>
+                        {currentUserTweets.map((item, index) => (    
+                                <div class="single-tweet" key={index}>
+                                    <h1>{this.findUsername(item.userid_id)}</h1>
+                                    <p>{item.tweet}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            )
+        } else {
+            return true;
+        }
     }
-
-    /*handleClick()   {
-        this.setState({ isMenuOpened: !this.state.isMenuOpened })
-    }*/
-
 }
--- src/components/middleware/SearchMiddleware.js
+++ src/components/middleware/SearchMiddleware.js
@@ -46,7 +46,6 @@ export default class SearchMiddleware extends Component {
 
         }
 
-
         return userTweet;
     }
 
